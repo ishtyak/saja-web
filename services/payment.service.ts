94359a78c3ab83163a25a13b5e42af8de14b6420
prashant-tlp <prashantsinghrajput56@gmail.com>
2026-06-17 13:23:06 +0530

    failed fix

--- services/payment.service.ts
+++ services/payment.service.ts
@@ -1,23 +1,23 @@
 export const createCheckoutSession = async (
-  payload: {
-    userId: string;
-    planId: string;
-  }
-) => {
-  const response = await fetch(
-    `${process.env.NODE_ENV == 'development' ? process.env.NEXT_PUBLIC_API_URL : process.env.NEXT_PUBLIC_API_URL_PROD}/api/payment/create-checkout-session`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
+    payload: {
+        userId: string;
+        planId: string;
     }
-  );
+) => {
+    const response = await fetch(
+        `${process.env.NODE_ENV == 'development' ? process.env.NEXT_PUBLIC_API_URL : process.env.NEXT_PUBLIC_API_URL_PROD}/api/payment/create-checkout-session`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+        }
+    );
 
-  if (!response.ok) {
-    throw new Error("Failed to create session");
-  }
+    if (!response.ok) {
+        throw new Error("Failed to create session");
+    }
 
-  return response.json();
+    return response.json();
 };
